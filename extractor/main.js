310e26152b7bef49e67538607a9efb40c03f68d9
Jan Werder <jan@werder.space>
2023-01-21 19:22:21 +0100

    Added a few month

--- extractor/main.js
+++ extractor/main.js
@@ -2,9 +2,16 @@ import got from "got";
 import fs from "fs";
 import * as cheerio from "cheerio";
 
-var year = new Date().getFullYear();
-
 var date = new Date().getMonth() + 1;
+
+if (process.argv.length > 2) {
+	var dateParsed = new Date(process.argv[2] + "-01");
+	if (dateParsed.toString() !== "Invalid Date") {
+		date = dateParsed.getMonth() + 1;
+	}
+}
+
+var year = dateParsed.getFullYear();
 var month = date.toString().length == 1 ? "0" + date : date;
 
 if (fs.existsSync(`../static/${year}-${month}.json`)) {
@@ -22,29 +29,39 @@ if (fs.existsSync(`../static/${year}-${month}.json`)) {
 	}
 }
 
-var monthText = new Date(`${year}-${month}-01`).toLocaleString("en-us", { month: "long" }).toLowerCase();
-const currentMonthUrl = `https://www.simpledailydrawing.com/drawing-prompts/${monthText}-${year}`;
+let monthText = new Date(`${year}-${month}-01`).toLocaleString("en-us", { month: "long" }).toLowerCase();
+let firstUrlPart = ["https://www.simpledailydrawing.com/drawing-prompts/", "https://www.simpledailydrawing.com/drawing-prompt-lists/"];
+let SecondUrlPart = [`${monthText}-${year}`];
 
-got(currentMonthUrl)
-	.then((response) => {
-		const $ = cheerio.load(response.body);
+let possibleUrls = firstUrlPart.map((first) => SecondUrlPart.map((second) => first + second)).flat();
 
-		//Parse all p tags, but catch the special case where the text is bold
-		var texts = $(".sqsrte-large")[0]
-			.next.children.filter((child) => child.type === "text")
-			.map((text) => (text.data.trim().substr(-1) === "•" ? text.data + text.next.children[0].data.trim() : text.data));
+//try to reach the url with the firstUrlPart
+possibleUrls.forEach((url) => {
+	got(url)
+		.then((response) => {
+			const $ = cheerio.load(response.body);
 
-		//Filter out the Share Alert
-		texts = texts.map((text) => text.replace("*** Share Alert** (see below)", ""));
+			//Parse all p tags, but catch the special case where the text is bold
+			try {
+				let texts = $(".sqsrte-large")[0]
+					.next.children.filter((child) => child.type === "text")
+					.map((text) => (text.data.trim().substr(-1) === "•" ? text.data + text.next.children[0].data.trim() : text.data));
 
-		//Now map it onto an object
-		var out = {};
-		texts.map((text) => (out[text.split("•")[0].trim()] = text.split("•")[1].trim()));
+				//Filter out the Share Alert
+				texts = texts.map((text) => text.replace("*** Share Alert** (see below)", ""));
 
-		//Write out to a json file
-		fs.writeFileSync(`../static/${year}-${month}.json`, JSON.stringify(out, null, '\t'));
-		console.log(out);
-	})
-	.catch((err) => {
-		console.log(`${year}-${month} URL does not exist. Exiting`);
-	});
+				//Now map it onto an object
+				let out = {};
+				texts.map((text) => (out[text.split("•")[0].trim()] = text.split("•")[1].trim()));
+
+				//Write out to a json file
+				fs.writeFileSync(`../static/${year}-${month}.json`, JSON.stringify(out, null, "\t"));
+				console.log(out);
+			} catch (error) {
+				console.log(`Error during extract: ${error}`);
+			}
+		})
+		.catch((err) => {
+			console.log(`${url} does not exist. Trying next url`);
+		});
+});
